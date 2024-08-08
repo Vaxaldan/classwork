916d8365fb6bf25e0d4cab42d621f9e399616916
Evan <evankendall68@gmail.com>
2024-08-07 22:46:02 -0400

    Number Analyzer update

---
+++ Unit-2-Fundamental-C#/Day 2 Sample Conditional Project/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader; //gives access to C# system stuff
+
+
+namespace Day_2_Sample_Conditional_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for an odd number between 11 and 17
+            //verify they have entered a correct value
+            //
+            // A number is odd if it is not evenly dvisible by 2
+            //   (is we divide it by 2 and the remainer is not 0, it's odd)
+            //
+            // C# has the modulus operator (%) which provides the remainder after the divide
+
+
+            Console.WriteLine("Please eneter an odd number between 11 and 17");
+            int theNum = int.Parse(Console.ReadLine()); //gets user input and converts to an int
+
+            // Show the user what we got from them
+            Console.WriteLine("You enetered: " + theNum);
+
+            // Verify the number is between 11 and 17
+            if (theNum >= 11 && theNum <= 17 && theNum % 2 != 0)
+            {
+                Console.WriteLine("The value IS between 11 and 17 and is ODD");
+            }
+            else if (theNum >= 11 && theNum <= 17 && theNum % 2 == 0)
+            { 
+                Console.WriteLine("The value IS between 11 and 17 and is NOT ODD");
+            }
+            else
+            { 
+                Console.WriteLine("The value IS NOT between 11 and 17");
+            }
+            
+
+            // Make visual studio keep the console windo open until we are done
+            Console.WriteLine("Please press eneter to end the program.");
+            Console.ReadLine();
+
+        
+        }
+    }
+}
---
+++ Unit-2-Fundamental-C#/Number-Analyzer-Lab/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+
+namespace Number_Analyzer_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello! What is your name?");
+            string user = Console.ReadLine(); // gets and stores the user's name
+
+            Console.WriteLine(user + ", please enter a number between 1 and 100.");
+            int userInput = int.Parse(Console.ReadLine()); // gets and stores the user's unput and converts it to int
+
+            Console.WriteLine(user + ", you enetered " + userInput);
+
+            if (userInput < 1 || userInput > 100)
+            {
+                Console.WriteLine(user + ", you enetered an invalid number");
+            }
+            else if (userInput >= 1 && userInput < 60 && userInput % 2 != 0) // checks to see if user's number is less greater than or equal to 1, less than 60 and if it is odd
+            {
+                Console.WriteLine(user + " ," + userInput + ", is odd and less than 60.");
+            }
+           
+            else if (userInput >= 2 && userInput <= 24 && userInput % 2 == 0) // checks if user's number is greater than or equal 2, and less than or equal to 24 and if it is even
+            {
+                Console.WriteLine(user + " ," + userInput + " is even and less than 25.");
+            }
+            else if (userInput >= 26 && userInput <= 60 && userInput % 2 == 0) // checks if user's number is greater than or equal to 26, and less than or equal to 60 and if it is even
+            {
+                Console.WriteLine(user + " ," + userInput + " is even and between 26 and 60 inclusive.");
+            }
+            else if (userInput > 60 && userInput % 2 == 0) // checks if the user's number is greater than 60 and if it is even
+            {
+                Console.WriteLine(user + " ," + userInput + " is even and greater than 60");
+            }
+            else if (userInput > 60 && userInput % 2 != 0) // checks if the user's number is greater than 60 and if it is odd
+            {
+                Console.WriteLine(user + " ," + userInput + " is odd and greater than 60");
+            }
+
+
+            Console.WriteLine("Please press ENTER to end program.");  //keeps visual studio open at the end of the program
+            Console.ReadLine();
+
+        }
+    }
+}
