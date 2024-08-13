3b3b72fcd46e353726066b1c44ce23dccaa755d4
Evan <evankendall68@gmail.com>
2024-08-13 18:48:30 -0400

    Frank update

---
+++ Labs/Dice-Lab/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Dice_Lab
+{
+    internal class Program
+    {
+        public static int GenerateRandomNumber(int minValue, int maxValue)
+        {
+            Random random = new Random();
+            return random.Next(minValue, maxValue + 1);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
