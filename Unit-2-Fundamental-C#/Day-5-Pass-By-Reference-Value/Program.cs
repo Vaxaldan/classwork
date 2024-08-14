83246d4f04161f734b5061c00f4bfcab5a893982
Frank Fella <[A[A[B[Bfrank.fella@simplilearn.net>
2024-08-14 15:09:39 -0700

    Instructor Push 08/14/2024 15:09:38

---
+++ Unit-2-Fundamental-C#/Day-5-Pass-By-Reference-Value/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Day_5_Pass_By_Reference_Value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 0;
+            int yourNum = 0;
+
+            Console.WriteLine("\n------ Calling method by value and by reference ------");
+            Console.WriteLine("Values before call to method: myNum=" + myNum + "  yourNum=" + yourNum);
+            method1(myNum, ref yourNum);
+            Console.WriteLine("Values after call to method: myNum=" + myNum + "  yourNum=" + yourNum);
+
+            Console.WriteLine("\nPlease press enter to end program");
+            Console.Read();
+
+        } // End of Main()
+
+        static void method1(int num1, ref int num2)
+        {
+            Console.WriteLine("At start of method, num1=" + num1 + "   num2=" + num2);
+            num1++;
+            num2++;
+            Console.WriteLine("At  end  of method, num1=" + num1 + "   num2=" + num2);
+        }
+
+    }// End of class Program
+} // End of namespace
