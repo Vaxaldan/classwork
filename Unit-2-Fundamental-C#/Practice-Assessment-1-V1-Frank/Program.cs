6e63b943334192cc99954cc81344672f080a6f3b
Frank Fella <[A[A[B[Bfrank.fella@simplilearn.net>
2024-08-20 08:33:02 -0700

    Instructor Push 08/20/2024 08:33:02

---
+++ Unit-2-Fundamental-C#/Practice-Assessment-1-V1-Frank/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;using System.IO;
+
+
+namespace practice_assessment_1_V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my Mock Assessment!");
+
+            // Test IsTheSame() method
+            //   1. with two equal values - expect method to return true
+            Console.WriteLine("Test with two equal values is: " + IsTheSame(-1, -1));
+
+            //   2. with two unequal value - expect method to return false
+            Console.WriteLine("Test with two unequal values is: " + IsTheSame(-1, 1));
+
+            // Test subtract method
+            //  1. Send a big number and smaller number
+            //  2. Send a smaller number and larger number
+            //  3. Send two numbers with same value
+
+           
+            Console.WriteLine("Test Subtract(4, 10): "  + Subtract(4, 10));
+            Console.WriteLine("Test Subtract(10, 10): " + Subtract(10, 10));
+
+            Console.WriteLine("Test FindBuildingType(3): " + FindBuildingType(3));
+            Console.WriteLine("Test FindBuildingType(1): " + FindBuildingType(1));
+            Console.WriteLine("Test FindBuildingType(0): " + FindBuildingType(0));
+
+            Console.WriteLine("Test FindBuildingType(4): " + FindBuildingType(4));
+            Console.WriteLine("Test FindBuildingType(10): " + FindBuildingType(10));
+            Console.WriteLine("Test FindBuildingType(4): " + FindBuildingType(5));
+
+            Console.WriteLine("Test FindBuildingType(11): " + FindBuildingType(11));
+            Console.WriteLine("Test FindBuildingType(49): " + FindBuildingType(49));
+            Console.WriteLine("Test FindBuildingType(33): " + FindBuildingType(33));
+
+            Console.WriteLine("Test FindBuildingType(50): " + FindBuildingType(50));
+            Console.WriteLine("Test FindBuildingType(51): " + FindBuildingType(51));
+
+            Console.WriteLine("Thanks for using my Mock Assessment!");
+
+        } // End of main
+
+        static string FindBuildingType(int floorAmount)
+        {
+            switch (floorAmount)
+            {
+                case int house when house <= 3:
+                {
+                    return "This is a house!";
+                }
+                case int officeBuilding when officeBuilding >= 4 && officeBuilding <= 10:
+                {
+                    return "This is a office building!";
+                }
+                case int skyscraper when skyscraper >= 11 && skyscraper <= 49:
+                {
+                    return "This is a skyscraper!";
+                }
+                case int superSkyscraper when superSkyscraper >= 50:
+                {
+                    return "This is a SUPER skyscraper!";
+                }
+                default:
+                {
+                    return "Invalid floor amount!";
+                }
+            }
+        }
+
+
+        static double Subtract(double num1, double num2)
+        {
+            /****** Overkill and not 100% correct (make the result -) *****
+            // if num1 is greater than num2 - subtract num2 from num1
+            //                      otherwise subtract num1 from num2
+            //                                and return the negative of the result
+            return (num1 > num2) ? num1 - num2 : -(num2 - num1);
+            ***************************************************************/
+            return num1 - num2;
+        }
+
+
+
+        // This method will receive two ints and return a bool
+        //     return 
+        //      type  name(parameters)
+         static bool IsTheSame(int num1, int num2)
+         {
+             if (num1 == num2)
+             {
+                 return true; // terminate the method and return value to the caller
+             }
+             return false;  // else was not required since only choices are available
+             /*
+             else
+             {
+                 return false;
+             } */
+
+             // Every expression results in a value
+             // the return statement returns a value
+             // ergo and expression may be coded on a return statement
+
+             // return num1 == num2;  // return the bool values from ==
+         }
+    } // End of class Program
+} // End of namespace
---
+++ Unit-3-Arrays-Collections-Exceptions/Day-7-Array-Example/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Day_5_Array_Example
+{
+    internal class Program
+    {
+        /**************************************************************************************
+         * This app will receive up to 10 numbers from the user
+         *
+         * After the user has indicated they have no more numbers to enter
+         *             or 10 numbers have been entered...
+         *               
+         * We will display each or the numbers entered with an indicator if number odd or even,
+         *                 their sum and average
+         *****************************************************************************************/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWelcome to my app!");       // Verify the app started
+
+            // Set up a loop to get 5 numbers, one at a time
+            for (int i = 0; i < 5; i++)
+            {
+                // Call the method to get a numeric value
+                double aValue = GetANumber();
+
+                // Display the value we got from the method
+                Console.WriteLine("Number entered was: " + aValue);
+            }
+            Console.WriteLine("\nThanks for using my app!"); // Verify the app ended
+
+            Console.WriteLine("\nPress enter to end program...");
+            Console.ReadLine();
+        } // End of Main()
+
+        /****************************************************************
+         * Helper methods used by Main()
+         ******************************************************************/
+        // method starts with a method signature:  return-type name(parameters)
+
+        // This method will get a numeric value from the user
+        // It must be static because it will be used by the static method Main() (more later)
+        // this method receives no parameters and returns a double (a double can also hold an int value)
+        static double GetANumber()
+        {
+            // define a variable for the return value
+            double theValue = 0;
+
+            // Ask the user for a numeric value and have them keep trying until we get one
+
+            bool isValidNumber = false;  // Determine is user entered a valid value
+
+            // Loop until we get a valid numeric value
+
+            do  // do loop is used so we ask the user for a number at least once
+            {
+                // Prompt the user to enter a numeric value
+                Console.WriteLine("Please enter a number");
+
+                // Get the input from the user
+                string userInput = Console.ReadLine();
+
+                try // We want to handle an Exception that might occur in this block of code
+                {
+                    // Convert the user input to a double
+                    theValue = double.Parse(userInput); // Could cause an Exception
+                    isValidNumber = true;  // if .Parse() worked we have a valid number
+                }
+                // catch (Exception exceptionBlock) will handle every Exception that can occur
+                catch (FormatException exceptionBlock) // Handle a FormatException in previous try block
+                {
+                    Console.WriteLine("\n----- Uh-oh Uh-oh Uh-oh ------");
+                    Console.WriteLine("There is problem with " + userInput);
+                    Console.WriteLine(exceptionBlock.Message); // Display the system message for the error
+                    Console.WriteLine("------ Uh-oh Uh-oh Uh-oh ------\n");
+                }
+            } while (!isValidNumber); // Loop while we don't have a valid number
+
+            // return the double value from the user input
+            return theValue;
+        } // End of getANumber() method
+    } // End of class Program
+} // End of namespace
