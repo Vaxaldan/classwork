d9047f8e1d870525728f719921a6582ac11f0791
Frank Fella <[A[A[B[Bfrank.fella@simplilearn.net>
2024-08-21 14:42:50 -0700

    Instructor Push 08/21/2024 14:42:49

---
+++ Unit-3-Arrays-Collections-Exceptions/Day-2-ArrayList-Example/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Day_2_ArrayList_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }  // End of Main()
+
+        /****************************************************************************************************
+         * This method will get a numeric value from the user
+         * It must be static because it will be used by the static method Main() (more later)
+         * this method receives no parameters and returns a double (a double can also hold an int value)
+        ****************************************************************************************************/
+        static double GetANumber()
+        {
+            // define a variable for the return value
+            double theValue = 0;
+
+            // Ask the user for a numeric value and have them keep trying until we get one
+
+            bool isValidNumber = false;  // Determine is user entered a valid value
+
+            // Loop until we get a valid numeric value
+
+            do  // do loop is used so we ask the user for a number at least once
+            {
+                // Prompt the user to enter a numeric value
+                Console.WriteLine("Please enter a number");
+
+                // Get the input from the user
+                string userInput = Console.ReadLine();
+
+                try // We want to handle an Exception that might occur in this block of code
+                {
+                    // Convert the user input to a double
+                    theValue = double.Parse(userInput); // Could cause an Exception
+                    isValidNumber = true;  // if .Parse() worked we have a valid number
+                }
+                // catch (Exception exceptionBlock) will handle every Exception that can occur
+                catch (FormatException exceptionBlock) // Handle a FormatException in previous try block
+                {
+                    Console.WriteLine("\n----- Uh-oh Uh-oh Uh-oh ------");
+                    Console.WriteLine("There is problem with " + userInput);
+                    Console.WriteLine(exceptionBlock.Message); // Display the system message for the error
+                    Console.WriteLine("------ Uh-oh Uh-oh Uh-oh ------\n");
+                }
+            } while (!isValidNumber); // Loop while we don't have a valid number
+
+            // return the double value from the user input
+            return theValue;
+        } // End of getANumber() method
+
+        /************************************************************************************
+         * return a boolean value to indicate if the user has more input
+         ***********************************************************************************/
+        static bool moreInput()
+        {
+            bool isThereInput = false;  // Hold teh return value 
+
+            string whatUserTyped = "";     // Hold what the user enters
+
+            bool getInput = true;   // Control the user interaction loop
+
+            do
+            {
+                // Ask the user if they have any numbers to enter (Y/N)
+                Console.WriteLine("Do you have any numbers to enter (Y/N)?");
+                whatUserTyped = Console.ReadLine();
+
+                whatUserTyped = whatUserTyped.ToUpper();
+
+                string firstChar = whatUserTyped.Substring(0, 1);
+
+                if (firstChar == "Y")
+                {
+                    getInput = false;
+                    isThereInput = true;
+                }
+                else
+                {
+                    if (firstChar == "N")
+                    {
+                        getInput = false;
+                        isThereInput = false;
+                    }
+                }
+            } while (getInput); // Loop while we get input
+
+            return isThereInput;
+        }
+
+    } // End of class Program
+} // End of namespace
---
+++ Unit-3-Arrays-Collections-Exceptions/Day-2-Multi-Dimensional-Arrays-Example/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Day_2_Multi_Dimensional_Arrays_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] bowlers = new int[5, 3];
+
+            for (int i = 0; i < bowlers.GetLength(0); i++)
+            {
+                Console.WriteLine("Please enter the 3 scores for bowler # " + (i + 1)
+                                                                            + " separated by \',\'");
+                string[] bowlerScores = Console.ReadLine().Split(',');
+
+                for (int j = 0; j < bowlers.GetLength(1); j++)
+                {
+                    bowlers[i, j] = int.Parse(bowlerScores[j]);
+                }
+            }
+
+            for (int i = 0; i < bowlers.GetLength(0); i++)
+            {
+                    Console.Write("\nBowler #" + (i+1) + " scores: ");
+
+                    for (int j = 0; j < bowlers.GetLength(1); j++)
+                    {
+                        Console.Write(bowlers[i,j] + " ");
+                    }
+
+                    Console.Write("  Total Score: " + sumScores(bowlers, i));
+
+                    Console.Write("  Average: " + sumScores(bowlers, i) / bowlers.GetLength(1));
+
+            }
+
+            Console.WriteLine("\n\nPress enter to end program...");
+            Console.ReadLine();
+
+        } // End of Main()
+
+        static int sumScores(int[,] anArray, int bowlerNumber)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < anArray.GetLength(1); i++)
+            {
+                sum += anArray[bowlerNumber,i];
+            }
+
+            return sum;
+        }
+    } // End class Program
+} // End namespace
