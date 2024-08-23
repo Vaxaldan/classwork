e1986929430bf748354c9133f79c75485adaadd8
Frank Fella <[A[A[B[Bfrank.fella@simplilearn.net>
2024-08-23 07:38:52 -0700

    Instructor Push 08/23/2024 07:38:52

---
+++ Unit-3-Arrays-Collections-Exceptions/Day-3-Dictionary-Example-Frank/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;  // Give me access to the systems generic collections stuff
+
+namespace Day_3_Dictionary_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*********************************************************
+             * Example use of a Dictionary
+             *********************************************************/
+
+            // Create a Dictionary for relate zip codes people live in
+            //
+            //  Key - Persons Name - Unique Id for an entry
+            //  (string)
+            //
+            //  value - Zip Code  - data associated with Key
+            //  (int)
+            //
+            // Syntax:   Dictionary<key-type, value-type> = new Dictionary<key-type, value-type)
+
+         // data-type<key-type, value-type> name = new data-type<key-type, value>();  
+            Dictionary<string, int> personInfo   = new Dictionary<string, int>();
+
+            // Add some people and their zip codes
+            //
+            // dictionaryName[key] = value;
+            //
+            // dictionaryName.Add(key, value) 
+
+            personInfo["Frank"]  = 85339;
+            personInfo["Evan"]   = 48009;
+            personInfo["Joshua"] = 15658;
+            personInfo["Ryan"]   = 49116;
+
+            personInfo.Add("Jenna", 85339);
+
+            // Specifying a key that already exists in the Dictionary
+            // When using [], the entry for the key is update
+            personInfo["Frank"] = 44143;
+
+            // Specifying a key that already exists in the Dictionary
+            // when using .Add() an exception is thrown if key already exists
+            // So to avoid this and still use .Add() check to see if teh key already
+            //       exists in Dictionary and only do teh .Add if it is not
+            // if the key value is NOT in the dictionary...
+            if (!personInfo.ContainsKey("Frank"))
+            {
+                personInfo.Add("Frank", 06082);  // Add it
+            }
+            else // If key IS already in the Dictionary
+            {
+                personInfo["Frank"] = 06082;    // Update its value
+            }
+
+            Console.WriteLine("Joshua lives in: " + personInfo["Joshua"]);
+
+            Console.Write("Whose zip code do you want? ");
+            string name = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(name + " lives in: " + personInfo[name]);
+            }
+            catch (KeyNotFoundException exceptionInfo)
+            {
+                Console.WriteLine("Error looking for: " + name);
+                Console.WriteLine(exceptionInfo.Message);
+            }
+
+            // If you want to walkthrough teh entire Dictionary with a foreach
+            //    You need to get all the keys in the Dictionary
+            //
+            // use the KeyValuePair object in the foreach
+            //
+            // KeyValuePair<key-type, value-type> name-for-dictionary-entry
+
+            foreach (KeyValuePair<string, int> anEntry in personInfo)
+            {
+                Console.WriteLine(anEntry.Key + " lives in zip code " + anEntry.Value);
+            }
+                 
+            Console.WriteLine("Please press enter to end program...");
+            Console.Read();
+        }
+    }
+}
