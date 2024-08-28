f3ac4e2f534a708ee9a487bad515ff9a5d7da632
Frank Fella <[A[A[B[Bfrank.fella@simplilearn.net>
2024-08-28 13:05:27 -0700

    Instructor Push 08/28/2024 13:05:26

---
+++ Unit-3-Arrays-Collections-Exceptions/Day-5-Linq-with-Class-Object/StarFleetPersonnel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Policy;
+
+namespace StarTrekStuff
+{
+    public class StarFleetPersonnel
+    {
+        public string Name { get;}
+        public string Rank { get;}
+        public string Assignment { get;}
+
+        public StarFleetPersonnel() {}
+
+        public StarFleetPersonnel(string personName, string theirRank, string theirAssignment)
+        {
+            Name       = personName;
+            Rank       = theirRank;
+            Assignment = theirAssignment;
+        }
+
+
+        /************************************************************************************
+         * Display a line from the list parsed into individual values
+         ************************************************************************************/
+        public void DisplayCrewMember()
+        {
+            Console.WriteLine($"\nName:\t{Name}\nCrew:\t{Assignment}\nRank:\t{Rank}");
+
+        } // End of DisplayLine()
+
+
+    } // End of class StarFleetPersonnel
+}  // End of namespace StarTrekStuff
