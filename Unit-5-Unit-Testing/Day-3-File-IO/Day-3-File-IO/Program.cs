565b3605bcffafd7aa9d07401c1f175c3011fcba
Frank Fella <[A[A[B[Bfrank.fella@simplilearn.net>
2024-09-19 15:20:01 -0700

    Instructor Push 09/19/2024 15:20:00

---
+++ Unit-5-Unit-Testing/Day-3-File-IO/Day-3-File-IO/Program.cs
@@ -0,0 +1,18 @@
+﻿using GeneralPurposeFunctions;
+namespace Day_3_File_IO
+{
+    internal class Program
+    {
+        private static CommonlyUsedFunctions _myFuncs = new CommonlyUsedFunctions();
+
+        static void Main(string[] args)
+        {
+            _myFuncs.WriteSeparatorLine("Welcome to a File I/O Example");
+
+
+            _myFuncs.PauseProgram();
+            _myFuncs.WriteSeparatorLine("Thanks for visiting a File I/O Example");
+
+        }
+    }
+}
